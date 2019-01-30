0235d027badab2c2dd5d8e4d6918abc8cc4c428b
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-01-30 12:13:47 +0100

    Disable matrixAutoUpdate

--- app/view/slot_anchors.js
+++ app/view/slot_anchors.js
@@ -16,6 +16,7 @@ class SlotAnchors { // eslint-disable-line no-unused-vars
     this.scene.traverse((obj) => {
       if (obj.userData.x3dType === 'Slot' && obj.userData.slotType === slotType && obj.children.length === 0) {
         var mesh = new THREE.Mesh(this.slotRepresentation, this.regularMaterial);
+        mesh.matrixAutoUpdate = false;
         mesh.name = 'slot representation';
         obj.add(mesh);
         this.slotRepresentationList.push(mesh);
