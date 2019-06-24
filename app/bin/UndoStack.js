e9830d4e47665303f3125aee010c688014224ba8
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2019-06-24 13:08:37 +0200

    Fix ESLint warnings

--- app/bin/UndoStack.js
+++ app/bin/UndoStack.js
@@ -1,8 +1,8 @@
 // Source: https://gist.github.com/dsamarin/3050311
 
-function UndoItem (perform, data) {
-	this.perform = perform;
-	this.data = data;
+function UndoItem(perform, data) {
+  this.perform = perform;
+  this.data = data;
 }
 
 /**
@@ -11,9 +11,9 @@ function UndoItem (perform, data) {
  **/
 
 function UndoStack(self) {
-	this.stack = [];
-	this.current = -1;
-	this.self = self;
+  this.stack = [];
+  this.current = -1;
+  this.self = self;
 }
 
 /**
@@ -22,48 +22,46 @@ function UndoStack(self) {
  * perform(false, data) -> Function which performs undo based on current state
  * data -> Argument passed to undo/redo functions
  **/
-UndoStack.prototype.push = function (perform, data) {
-	this.current++;
+UndoStack.prototype.push = function(perform, data) {
+  this.current++;
 
-	// We need to invalidate all undo items after this new one
-	// or people are going to be very confused.
-	this.stack.splice(this.current);
-	this.stack.push(new UndoItem(perform, data));
+  // We need to invalidate all undo items after this new one
+  // or people are going to be very confused.
+  this.stack.splice(this.current);
+  this.stack.push(new UndoItem(perform, data));
 };
 
-UndoStack.prototype.undo = function () {
-	var item;
+UndoStack.prototype.undo = function() {
+  var item;
 
-	if (this.current >= 0) {
-		item = this.stack[this.current];
-		item.perform.call(this.self, false, item.data);
-		this.current--;
-	} else {
-		throw new Error("Already at oldest change");
-	}
+  if (this.current >= 0) {
+    item = this.stack[this.current];
+    item.perform.call(this.self, false, item.data);
+    this.current--;
+  } else
+    throw new Error('Already at oldest change');
 };
 
-UndoStack.prototype.redo = function () {
-	var item;
+UndoStack.prototype.redo = function() {
+  var item;
 
-	item = this.stack[this.current + 1];
-	if (item) {
-		item.perform.call(this.self, true, item.data);
-		this.current++;
-	} else {
-		throw new Error("Already at newest change");
-	}
+  item = this.stack[this.current + 1];
+  if (item) {
+    item.perform.call(this.self, true, item.data);
+    this.current++;
+  } else
+    throw new Error('Already at newest change');
 };
 
-UndoStack.prototype.canUndo = function () {
-	return this.current >= 0;
-}
+UndoStack.prototype.canUndo = function() {
+  return this.current >= 0;
+};
 
-UndoStack.prototype.canRedo = function () {
-	return this.stack[this.current + 1] !== undefined;
-}
+UndoStack.prototype.canRedo = function() {
+  return this.stack[this.current + 1] !== undefined;
+};
 
-UndoStack.prototype.invalidateAll = function () {
-	this.stack = [];
-	this.current = -1;
+UndoStack.prototype.invalidateAll = function() {
+  this.stack = [];
+  this.current = -1;
 };
